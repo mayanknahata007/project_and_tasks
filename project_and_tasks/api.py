57ee776722d4f3cc60f5351a6cc84d4b039c9509
Mayank Nahata <mayanknahata007@gmail.com>
2022-11-17 22:04:54 +0100

    Added more data to the tasks shown in the header section of project

--- project_and_tasks/api.py
+++ project_and_tasks/api.py
@@ -1,14 +1,20 @@
 import frappe
 
-
-
 @frappe.whitelist()
 def get_tasks():
     project = frappe.form_dict.get('project')
-
-    all_tasks = frappe.get_all('Task', filters ={'project':project})
+    global a,b,c
     a = []
+    b,c = [],[]
+    all_tasks = frappe.get_all('Task', filters ={'project':project})
     for i in all_tasks:
         a.append((frappe.get_doc('Task',i)).name)
+        b.append((frappe.get_doc('Task',i)).subject)
+        c.append((frappe.get_doc('Task',i)).status)
 
-    frappe.response.message = a
+    d = {
+        "name":a,
+        "subject":b,
+        "status1":c
+    }
+    frappe.response.message = d
--- project_and_tasks/public/js/project.js
+++ project_and_tasks/public/js/project.js
@@ -1,5 +1,6 @@
 frappe.ui.form.on('Project', {
 	refresh(frm) {
+        console.log("Bhai ka app")
         intro(frm)
 		set_status(frm)
 	}
@@ -7,12 +8,15 @@ frappe.ui.form.on('Project', {
 
 function intro(frm)
 {
-    frm.set_intro(('{0}',
-    [
-        '<h3>Tasks for this Project</h3>'
-        ]
-    )
+    if(!frm.is_new())
+    {
+        frm.set_intro(('{0}',
+        [
+            '<h3>Tasks for this Project</h3>'
+            ]
         )
+            )
+    }
 }
 
 function set_status(frm)
@@ -22,13 +26,17 @@ function set_status(frm)
     }
     frappe.call('project_and_tasks.api.get_tasks', params).then((r) => {
         let total = r.message;
-        let size = total.length;
+        let name = total.name;
+        let status = total.status1;
+        let subject = total.subject;
+        let size = name.length;
         for(let i = 0; i< size; i++)
         {
-            if (!frm.doc.description) {
-                frm.set_intro(('{0}',
+            if (!frm.doc.description && !frm.is_new()) {
+                frm.set_intro(('',
                       [
-                        `<a href="/app/task/${total[i]}">${i+1}:${total[i]}</a>`,
+                        `<a href="/app/task/${name[i]}">${i+1}:${subject[i]} - ${name[i]}</a>`,
+                        `<p>${status[i]}</p>`,
                       ]), 'red');
             }
         }
